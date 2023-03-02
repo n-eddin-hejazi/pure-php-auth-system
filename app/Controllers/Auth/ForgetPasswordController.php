95405a810f8b333a652b62edc08b09c21e462e57
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-03 02:38:46 +0300

    add token - forget password

--- app/Controllers/Auth/ForgetPasswordController.php
+++ app/Controllers/Auth/ForgetPasswordController.php
@@ -34,6 +34,7 @@ class ForgetPasswordController
         include 'database/db_connection.php';
         $stmt = $db->prepare("SELECT * FROM `users` WHERE `email` = ?");
         $stmt->execute([$this->email]);
+        
 
         // if email not exist, return back
         if (!$stmt->rowCount()) {
@@ -44,16 +45,49 @@ class ForgetPasswordController
 
         // if email exist, send email
         if ($stmt->rowCount()) {
+            $user = $stmt->rowCount() ? $stmt->fetch() : null;
             $subject = env('APP_NAME') . " Account recovery information";
-            $HTML_message = file_get_contents(view_path() . 'emails/forget-passowrd-email.view.php');
+
+            $email = $this->email;
+            $token = $this->generateUniqueToken();
+            $url = main_url() . "/reset-passowrd?email={$this->email}&token={$token}";
+            $HTML_message = file_get_contents(view_path() . 'emails/forget-passowrd-email.html');
+            $HTML_message = str_replace('{url}', $url, $HTML_message);
+
             if(Mail::sendMail($this->email, $subject, $HTML_message)){
-                session()->setFlash('success', "You will receive an email if your email is registered.");
-                return to('login');
+                // Insert the email and token into the password_resets table
+                $stmt = $db->prepare('INSERT INTO password_resets (email, token) VALUES (?, ?)');
+                $result = $stmt->execute([$email, $token]);
+                if ($result) {
+                    session()->setFlash('success', "You will receive an email if your email is registered.");
+                    return to('login');
+                } else {
+                    session()->setFlash('fail', "There is an error, please try again later!.");
+                    return back();
+                }
+
             }else{
                 session()->setFlash('fail', "There is an error, please try again later!.");
                 return back();
             }
             
+        }
+    }
+
+    private function generateUniqueToken()
+    {
+        $token = bin2hex(random_bytes(25));
+
+        include 'database/db_connection.php';
+        $stmt = $db->prepare('SELECT * FROM password_resets WHERE token = ?');
+        $stmt->execute([$token]);
+        $result = $stmt->fetch();
+        if ($result) {
+            // Token already exists in the database
+            return $this->generateUniqueToken();
+        } else {
+            // Token does not exist in the database
+            return $token;
         }
     }
 
