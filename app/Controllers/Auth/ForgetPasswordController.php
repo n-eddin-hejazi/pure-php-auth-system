2afc7f739f3fa8acb7bdbc54f09f6ca94518b1cb
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-02 21:27:42 +0300

    make validation of forget password

--- app/Controllers/Auth/ForgetPasswordController.php
+++ app/Controllers/Auth/ForgetPasswordController.php
@@ -4,8 +4,78 @@ namespace App\Controllers\Auth;
 
 class ForgetPasswordController
 {
+
+    private string $email;
     public function index()
     {
+        ifAuth();
         return view('auth.forget-password');
+    }
+
+    public function forgetPassword()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['email'])) {
+                // assign preperties
+                $this->email = htmlspecialchars(strip_tags($_POST['email']));
+
+                // check the validations
+                $this->validation();
+
+                // send mail
+                $this->sendEmail();
+
+            }
+        }
+    }
+
+    private function sendEmail()
+    {
+        // email validation
+        include 'database/db_connection.php';
+        $stmt = $db->prepare("SELECT * FROM `users` WHERE `email` = ?");
+        $stmt->execute([$this->email]);
+
+        // if email not exist, return back
+        if (!$stmt->rowCount()) {
+            session()->setFlash('success', "You will receive an email if your email is registered.");
+            return back();
+        }
+
+        // if email exist, send email
+        if ($stmt->rowCount()) {
+            session()->setFlash('success', "You will receive an email if your email is registered.");
+            return back();
+        }
+    }
+
+    private function validation()
+    {
+        $email_errors = $this->emailValidation();
+        if (!empty($email_errors)) {
+            session()->setFlash('email_errors', $email_errors);
+        }
+
+        if (!empty($email_errors)) {
+            session()->setFlash('email', $this->email);
+            return back();
+        }
+
+    }
+
+    private function emailValidation()
+    {
+        $email_errors = [];
+        // email validation
+        if (empty($this->email)) {
+            $email_errors[] = "The email field is required.";
+        }
+
+        // email validation
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL) || strlen($this->email) < 6 || strlen($this->email) > 40) {
+            $email_errors[] = "Invalid email.";
+        }
+
+        return $email_errors;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,20 @@ use App\Controllers\HomeController;
      // homae page route
      Route::get('', [HomeController::class, 'home']);
 
-     // register page route
+     // register page routes
      Route::get('register', [RegisterController::class, 'index']);
      Route::post('register', [RegisterController::class, 'store']);
 
-     // login page route
+     // login page routes
      Route::get('login', [LoginController::class, 'index']);
      Route::post('login', [LoginController::class, 'store']);
 
      // logout page route
      Route::get('logout', [LogoutController::class, 'index']);
 
-
+     // forget password page routes
      Route::get('forget-password', [ForgetPasswordController::class, 'index']);
+     Route::post('forget-password', [ForgetPasswordController::class, 'forgetPassword']);
 
 
      
